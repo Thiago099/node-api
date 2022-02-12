d525f9908a232ef2879d316952fc92c50f6c9878
Thiago Kaique <66787043+Thiago099@users.noreply.github.com>
2022-02-12 18:17:20 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `teste` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`test` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`teste` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
